21b9204ed9cf90f12d1f12193bab9136cfba1fc9
Chris Snow <csnow@Chriss-MacBook-Pro.local>
2014-10-21 13:58:28 -0400

    Initial Commit

---
+++ Image Filter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image Filter
+//
+//  Created by Chris Snow on 10/21/14.
+//  Copyright (c) 2014 Chris Snow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
